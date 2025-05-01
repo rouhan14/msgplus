343fe35ce0b54d7930a909d31744355abe06482a
Muhammad Rouhan Faisal <rfaisal.bscs21seecs@seecs.edu.pk>
2025-05-01 17:48:06 +0500

    First commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const http = require("http")
+
+const app = express()
+const server = http.createServer(app)
+
+const { Server } = require("socket.io")
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(`User is connected with id: ${socket.id}`)
+})
+
+server.listen(3001, () => {
+    console.log("Server is running on port 3001")
+})
